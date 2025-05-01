9da504ab563ac0541d3e5ab5fbf76ca1cf269d70
Graham Brown <grhbwn@gmail.com>
2025-04-30 23:28:33 -0400

    move all the stuff out to plots component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-import { useEffect, useMemo, useState } from "react";
 import { Plots } from "~/welcome/plots";
-import { useProducer } from "~/hooks/useProducer";
-import UPlotReact from "uplot-react";
-import { data } from "react-router";
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,70 +8,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const dummyPlugin = (): uPlot.Plugin => ({
-  hooks: {
-    init(u: uPlot, opts: uPlot.Options) {
-      void u;
-      void opts;
-    },
-  },
-});
-
 export default function Home() {
-  const [options, setOptions] = useState<uPlot.Options>(
-    useMemo(
-      () => ({
-        title: "Chart",
-        width: 400,
-        height: 300,
-        series: [
-          {
-            label: "Time",
-          },
-          {
-            label: "Value",
-            points: { show: true },
-            stroke: "blue",
-            fill: "blue",
-          },
-        ],
-        plugins: [dummyPlugin()],
-        scales: { x: { time: true } },
-      }),
-      []
-    )
-  );
-  const producer1Data = useProducer("1");
-  // useProducer("2");
-  // useProducer("3");
-  // useProducer("4");
-  // if (producer1Data !== undefined) {
-  //   debugger;
-  // }
-
-  if (producer1Data === undefined) {
-    return <div>loading...</div>;
-  }
-
-  const dataX = producer1Data?.reduce((acc, curr) => {
-    const unixTs = new Date(curr["timestamp"]).getTime() / 1000;
-    acc.push(unixTs);
-    return acc;
-  }, []);
-  const dataY = producer1Data?.reduce((acc, curr) => {
-    acc.push(curr["value"]);
-    return acc;
-  }, []);
-
-  const data = [dataX, dataY];
-  return (
-    <UPlotReact
-      key="hooks-key"
-      options={options}
-      data={data}
-      //target={root}
-      onDelete={(/* chart: uPlot */) => console.log("Deleted from hooks")}
-      onCreate={(/* chart: uPlot */) => console.log("Created from hooks")}
-    />
-  );
+  return <Plots></Plots>;
 }
